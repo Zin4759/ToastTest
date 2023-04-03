1ed16745bdfc4d39d695ed750919fc0c2f4a79e0
Zin4759 <vnaaiu7614@outlook.kr>
2023-04-03 20:49:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/buttonevents/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.buttonevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "장비를 정지합니다",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
